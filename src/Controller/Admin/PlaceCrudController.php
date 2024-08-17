e502ab07dc5675586e25bfd620a874a955791b21
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-08-17 16:16:26 +0000

    feat(controller) set Place Crud controller

---
+++ src/Controller/Admin/PlaceCrudController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Place;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class PlaceCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Place::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Lieu')
+            ->setEntityLabelInPlural('Lieux')
+            ->setPageTitle('new', "Création d'un lieu")
+            ->setPageTitle('edit', "Édition d'un lieu")
+            ->setSearchFields(['name', 'city', 'postcode'])
+        ;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
