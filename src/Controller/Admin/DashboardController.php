24f1ad303bf48a37c711276023c76a52935cb60b
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-08-17 16:15:42 +0000

    refactor(controller) add menu to dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Century;
+use App\Entity\Picture;
+use App\Entity\Place;
+use App\Entity\Tag;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        // return parent::index();
+        return $this->render('admin/dashboard.html.twig', []);
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -35,12 +42,26 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Workspace');
+            ->setTitle('Histotrip - administration')
+
+        ;
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
+        yield MenuItem::linkToCrud('Lieux', 'fa-solid fa-location-dot', Place::class);
+        yield MenuItem::linkToCrud('Categories', 'fas fa-icons', Category::class);
+        yield MenuItem::linkToCrud('Tags', 'fa-solid fa-tags', Tag::class);
+        yield MenuItem::linkToCrud('Siècles', 'fa-solid fa-calendar', Century::class);
+        yield MenuItem::linkToCrud('Images', 'fa-solid fa-image', Picture::class);
+    }
+
+    public function configureCrud(): Crud
+    {
+        return Crud::new()
+            ->setPaginatorPageSize(30)
+            ->hideNullValues()
+        ;
     }
 }
