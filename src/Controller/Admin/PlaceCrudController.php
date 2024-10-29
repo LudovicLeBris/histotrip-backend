d00ff471dcc4895150edb6427f8d114951af8c28
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-10-29 14:33:24 +0000

    refactor(controller) change form layout

--- src/Controller/Admin/PlaceCrudController.php
+++ src/Controller/Admin/PlaceCrudController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Place;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
@@ -37,24 +40,28 @@ class PlaceCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
+            FormField::addFieldset('Titre'),
             TextField::new('name', "Nom"),
             SlugField::new('slug', "slug")->setTargetFieldName('name'),
             TextField::new('subtitle', "Sous-titre")->hideOnIndex(),
             BooleanField::new('isValid', "Lieu approuvé"),
             NumberField::new('rating', "Note")->hideOnForm(),
+            FormField::addFieldset('Localisation')->collapsible()->renderCollapsed(),
             TextField::new('address', "Adresse")->hideOnIndex(),
             TextField::new('postcode', "Code postal"),
             TextField::new('city', "Ville"),
             TextField::new('country', "Pays"),
-            NumberField::new('latitude', "Latitude")->hideOnIndex(),
-            NumberField::new('longitude', "Longitude")->hideOnIndex(),
+            NumberField::new('latitude', "Latitude")->hideOnIndex()->setColumns(3),
+            NumberField::new('longitude', "Longitude")->hideOnIndex()->setColumns(3),
+            FormField::addFieldset('Informations pratiques')->collapsible()->renderCollapsed(),
             UrlField::new('website', "Site internet")->hideOnIndex(),
             TextField::new('phone', "Téléphone")->hideOnIndex(),
             TextEditorField::new('description', "Description")->hideOnIndex(),
             TextEditorField::new('price', "Prix")->hideOnIndex(),
             TextEditorField::new('openingHours', "Heures d'ouvertures")->hideOnIndex(),
-            BooleanField::new('accessibility', "Accès handicapés")->hideOnIndex(),
-            BooleanField::new('guidedTour', "Visite guidée")->hideOnIndex(),
+            BooleanField::new('accessibility', "Accès handicapés")->hideOnIndex()->setColumns(3),
+            BooleanField::new('guidedTour', "Visite guidée")->hideOnIndex()->setColumns(3),
+            FormField::addFieldset('Filtres')->collapsible(),
             AssociationField::new('categories', "Catégories")->hideOnIndex()->setSortProperty('category'),
             AssociationField::new('centuries', "Siècles - périodes")->hideOnIndex(),
             AssociationField::new('tags', "Tags")->hideOnIndex(),
@@ -68,6 +75,14 @@ class PlaceCrudController extends AbstractCrudController
         $place->setCreatedAt(new \DateTimeImmutable());
 
         return $place;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_NEW, Action::SAVE_AND_CONTINUE)
+        ;
     }
 
 }
